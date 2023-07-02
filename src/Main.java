51a0511a203395c8e7528056a72e18a88a292b99
ahmetulvi <47754871+ahmetulvi@users.noreply.github.com>
2023-07-02 19:58:21 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import  java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter size of the array: ");
+        int size= sc.nextInt();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int[] arr=new int[size];
+        System.out.println("Please enter elements of the array:");
+        for (int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
         }
+        int temp;
+        for (int i=0; i<arr.length; i++){
+            for (int j=i+1; j<arr.length;j++){
+                if (arr[j]<arr[i] ){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted array elements: " + Arrays.toString(arr));
+
+
     }
+
 }
